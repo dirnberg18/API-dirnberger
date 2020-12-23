7f501809be3d3419c29477ee80c8ae230ac35813
Marlene <marlene.dirnberger@edu.fh-joanneum.at>
2020-12-24 01:06:45 +0200

    adjust database.js

--- database.js
+++ database.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool
 
 const connection = new Pool({
-  user: 'netuser',
-  host: 'localhost',
-  database: 'netdb',
-  password: 'netpass',
+  user: 'gfrexfxdzvtcxl',
+  host: 'ec2-54-217-206-236.eu-west-1.compute.amazonaws.com',
+  database: 'dfobept1pa8mbi',
+  password: '9495d1554370c3b37c989135ea6d0836c39b62c03a7ff964475d15a493dcf167',
   port: 5432,
 })
 module.exports = connection;
